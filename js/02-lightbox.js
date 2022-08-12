a7f4859d4ce288c693b1ec1d3c8eba98b76f9649
Darymee <danieledaniele3005@gmail.com>
2022-08-12 20:55:41 +0200

    add task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,5 +1,6 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+console.log(galleryItems);
 
 const imageContainer = document.querySelector(".gallery");
 const imageMarkup = creacteGalleryMarkup(galleryItems);
@@ -7,9 +8,9 @@ const imageMarkup = creacteGalleryMarkup(galleryItems);
 function creacteGalleryMarkup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
-      return `<a class="gallery__item" href="${original}">
+      return `<li class="gallery__li"><a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>`;
+</a></li>`;
     })
     .join("");
 }
@@ -18,6 +19,14 @@ imageContainer.insertAdjacentHTML("beforeend", imageMarkup);
 
 imageContainer.addEventListener("click", onGalleryClick);
 
+function onGalleryClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+}
+
 var lightbox = new SimpleLightbox(".gallery a", {
-  /* options */
+  captionsData: "alt",
+  captionDelay: 250,
 });
