8a4ae604488ec0a98e80e73435db18e90cab737e
Darymee <danieledaniele3005@gmail.com>
2022-08-12 19:16:51 +0200

    Task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const imageContainer = document.querySelector(".gallery");
+const imageMarkup = creacteGalleryMarkup(galleryItems);
+
+function creacteGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+
+imageContainer.insertAdjacentHTML("beforeend", imageMarkup);
+
+imageContainer.addEventListener("click", onGalleryClick);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  /* options */
+});
