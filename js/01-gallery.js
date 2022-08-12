540820c2cc17340102d7da0bb59a19a4b9d8a9e3
Darymee <danieledaniele3005@gmail.com>
2022-08-12 12:54:03 +0200

    Add markup function

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const imageContainer = document.querySelector(".gallery");
+const imageMarkup = creacteGalleryMarkup(galleryItems);
+
+function creacteGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+imageContainer.insertAdjacentHTML("beforeend", imageMarkup);
